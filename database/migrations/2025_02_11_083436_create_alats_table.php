218ee3a3a4281d2cc405d1f5c0e8c1380e6582bb
IAMyanuar <ilhamyanuartaufiki27@gmail.com>
2025-02-11 16:08:08 +0700

    penambahan db

---
+++ database/migrations/2025_02_11_083436_create_alats_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama',45);
+            $table->string('satuan',45);
+            $table->string('foto',100);
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alats');
+    }
+};
